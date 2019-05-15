ed26df38f04a4312a065a433b646752a4eb6c462
Ricard Derheim <ricard.derheim@ars-ars.de>
2019-05-15 08:15:34 +0200

    added exmaple for writing custom decoders

---
+++ examples/custom-decoders.ts
@@ -0,0 +1,33 @@
+import * as D from './../src'
+import { isString } from './../src';
+
+type Superpower =  'freeze' | 'burn' | 'flash'
+
+type Hero = {
+  name: string
+  superpower: Superpower
+  hp: number | undefined
+}
+
+const json = `
+  {
+    "name": "Decodeman",
+    "superpower": "burn",
+    "hp": 100.10
+  }
+`
+
+const isSuperpower: D.Assert<Superpower> = (input: any): input is Superpower =>
+  [ 'freeze', 'burn', 'flash' ].some(x => x === input)
+
+const superpower = (): D.Decoder<Superpower> => (input: any): D.DecoderResult<Superpower> =>
+  isSuperpower(input) ? D.ok(input) : D.fail()
+
+const decodeHero = D.object<Hero>({
+  name: D.string(),
+  superpower: superpower(),
+  hp: D.optional(D.number())
+})
+
+const decodeHeroResult = decodeHero(JSON.parse(json))
+console.log(decodeHeroResult)
